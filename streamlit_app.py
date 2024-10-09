0affa2311bbd20196c32e0b8d92473dbff851654
lupes237 <luca.schwieren@googlemail.com>
2024-10-09 11:59:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
-import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 # Funktion zum Extrahieren von Tarifinformationen
 def get_tarif(tarif, options):
@@ -30,4 +30,83 @@ def get_tarif(tarif, options):
     zahlungsfrequenz = tarif.find_all(class_="price__Period-sc-19hw2m6-2")
     zahlungsfrequenz = [z.get_text() for z in zahlungsfrequenz] if zahlungsfrequenz else ['Keine Angabe']
 
-    rest
+    rest = tarif.find_all(class_="badgeLabels__BadgeLabelWrapper-sc-uavc85-1")
+    rest_texts = ";".join([r.get_text() for r in rest]) if rest else 'keine'
+
+    current_date = datetime.now().strftime("%d-%m-%Y")
+
+    return {
+        'Anbietername': anbietername,
+        'Abrufdatum': current_date,
+        'Region': options['region'],
+        'Person': options['person'],
+        'Tarifname': tarifname,
+        'Beitrag': beitrag,
+        'Zahlungsfrequenz': zahlungsfrequenz,
+        'UmweltRabatt': options['umweltrabatt'],
+        'Alter': int(options['alter']),
+        'Reduzierung': rabatt,
+        'Pannenhilfe': pannenhilfe,
+        'Ersatzwagen': ersatzwagen,
+        'Abschleppen': abschleppen,
+        'Krankenruecktransport': krankenruecktransport,
+        'AndereInformationen': rest_texts,
+        'PLZ': options['plz'],
+        'AbrufUrl': options['url']
+    }
+
+def get_all_tarifs_by_url(options):
+    response = requests.get(options['url'])
+    soup = BeautifulSoup(response.content, 'html.parser')
+    tarife = soup.select(".desktop__ProviderContainer-sc-1i3s93n-1")
+    tarif_list = [get_tarif(tarif, options) for tarif in tarife]
+    return tarif_list
+
+def generate_url(region, g_person="Single", fa="nein", beruf="Employee", umwelt_rabatt="nein", plz="40210", alter="30"):
+    current_date = (datetime.now() + timedelta(days=1)).strftime("%d-%m-%Y")
+    return f"https://kfz-schutzbrief.check24.de/c24-sb-checkin/results?insuredScope={region}&product=SB&insuranceStartDate={current_date}&driverLicenceLessThanOneYear={'true' if fa == 'ja' else 'false'}&employmentStatus={beruf}&environmentalDiscount={'true' if umwelt_rabatt == 'ja' else 'false'}&familyStatus={g_person}&postalCode={plz}&severelyDisabled=false&age={alter}"
+
+# Streamlit App
+st.title("KFZ Schutzbrief Tarife vergleichen")
+
+# Input-Felder für die Benutzereingabe
+region = st.selectbox("Wähle eine Region:", ["Germany", "Europe", "World"])
+person = st.selectbox("Wähle eine Personengruppe:", ["Single", "Pair", "Family"])
+plz = st.text_input("Postleitzahl:", "40210")
+umweltrabatt = st.selectbox("Umwelrabatt:", ["ja", "nein"])
+alter = st.slider("Alter:", 18, 31)
+
+# Button zum Abrufen der Daten
+if st.button("Tarife abrufen"):
+    url = generate_url(region, g_person=person, umwelt_rabatt=umwelrabatt, plz=plz, alter=alter)
+    options = {
+        'region': region,
+        'person': person,
+        'plz': plz,
+        'url': url,
+        'umweltrabatt': umwelrabatt,
+        'alter': alter
+    }
+    tarifs = get_all_tarifs_by_url(options)
+    df_tarifs = pd.DataFrame(tarifs)
+
+    # Ergebnisse anzeigen
+    st.write(f"Anzahl der gefundenen Tarife: {len(df_tarifs)}")
+    st.dataframe(df_tarifs)
+
+    # Diagramm erstellen
+    if not df_tarifs.empty:
+        # Beispiel: Preisdiagramm
+        df_tarifs['Beitrag'] = df_tarifs['Beitrag'].apply(lambda x: float(re.sub(r'[^\d.]', '', x[0])) if isinstance(x, list) and x else 0)
+        plt.figure(figsize=(10, 5))
+        plt.bar(df_tarifs['Tarifname'], df_tarifs['Beitrag'], color='blue')
+        plt.xlabel('Tarifname')
+        plt.ylabel('Beitrag in Euro')
+        plt.title('Tarife vergleichen')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        st.pyplot(plt)
+        
+    # Option zum CSV-Download
+    csv = df_tarifs.to_csv(index=False, sep=";")
+    st.download_button(label="CSV herunterladen", data=csv, file_name="KFZ-Schutzbrief-Tarife.csv", mime="text/csv")
