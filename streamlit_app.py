752d263fb7dbbdc54b73f1b36a8b8288ed08ff4a
lupes237 <luca.schwieren@googlemail.com>
2024-10-09 12:42:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ import plotly.graph_objs as go
 # Funktion zum Extrahieren von Tarifinformationen
 def get_tarif(tarif, options):
     tarifname = tarif.find(class_="logo__TariffName-sc-1jenrlw-3").get_text()
-
     anbietername = tarif.find(class_="badgeLabelsWithIcon__BadgeLabel-sc-18wjrn2-2").get('data-tooltip-id')
     anbietername = re.search(r'BadgeLabelWithIcon-([\w\s/]+)-', anbietername).group(1) if anbietername else 'Unbekannt'
 
@@ -18,7 +17,8 @@ def get_tarif(tarif, options):
 
     v = tarif.find_all(class_="badgeLabelsWithIcon__Title-sc-18wjrn2-3")
     v_texts = [v_elem.get_text() for v_elem in v]
-
+    
+    # Extrahiere weitere Informationen
     pannenhilfe = next((s.replace("Pannenhilfe ", "") for s in v_texts if "Pannenhilfe" in s), 'Nein')
     ersatzwagen = next((s.replace("Ersatzwagen ", "") for s in v_texts if "Ersatzwagen" in s), 'Nein')
     abschleppen = 'Ja' if any("Abschleppen des Fahrzeugs" in s for s in v_texts) else 'Nein'
@@ -26,12 +26,9 @@ def get_tarif(tarif, options):
 
     rabatt_elem = tarif.find(class_="price__CrossedOutPercentage-sc-19hw2m6-5")
     rabatt = rabatt_elem.get_text().replace("-", "") if rabatt_elem else 'keine'
-
+    
     zahlungsfrequenz = tarif.find_all(class_="price__Period-sc-19hw2m6-2")
     zahlungsfrequenz = [z.get_text() for z in zahlungsfrequenz] if zahlungsfrequenz else ['Keine Angabe']
-
-    rest = tarif.find_all(class_="badgeLabels__BadgeLabelWrapper-sc-uavc85-1")
-    rest_texts = ";".join([r.get_text() for r in rest]) if rest else 'keine'
 
     current_date = datetime.now().strftime("%d-%m-%Y")
 
@@ -50,7 +47,6 @@ def get_tarif(tarif, options):
         'Ersatzwagen': ersatzwagen,
         'Abschleppen': abschleppen,
         'Krankenruecktransport': krankenruecktransport,
-        'AndereInformationen': rest_texts,
         'PLZ': options['plz'],
         'AbrufUrl': options['url']
     }
@@ -59,8 +55,7 @@ def get_all_tarifs_by_url(options):
     response = requests.get(options['url'])
     soup = BeautifulSoup(response.content, 'html.parser')
     tarife = soup.select(".desktop__ProviderContainer-sc-1i3s93n-1")
-    tarif_list = [get_tarif(tarif, options) for tarif in tarife]
-    return tarif_list
+    return [get_tarif(tarif, options) for tarif in tarife]
 
 def generate_url(region, g_person="Single", fa="nein", beruf="Employee", umwelt_rabatt="nein", plz="40210", alter="30"):
     current_date = (datetime.now() + timedelta(days=1)).strftime("%d-%m-%Y")
@@ -100,25 +95,18 @@ if st.button("Tarife abrufen"):
         # Filtern der doppelten Anbieterwerte nur für ACV Komfort und ACE Comfort+
         df_acv_komfort = df_tarifs[df_tarifs['Anbietername'] == 'ACV Komfort']
         df_ace_comfort_plus = df_tarifs[df_tarifs['Anbietername'] == 'ACE Comfort+']
-        
-        # Alle anderen Anbieter beibehalten
         df_rest = df_tarifs[~df_tarifs['Anbietername'].isin(['ACV Komfort', 'ACE Comfort+'])]
 
-        # Doppelte herausfiltern, wobei der Tarif mit dem niedrigsten Beitrag genommen wird
         df_acv_komfort = df_acv_komfort.sort_values(by='Beitrag').drop_duplicates(subset='Tarifname', keep='first')
         df_ace_comfort_plus = df_ace_comfort_plus.sort_values(by='Beitrag').drop_duplicates(subset='Tarifname', keep='first')
 
-        # Kombinieren der DataFrames
-        df_tarifs_filtered = pd.concat([df_rest, df_acv_komfort, df_ace_comfort_plus])
-
-        # Sortiere nach Beitrag (Höhe)
-        df_tarifs_filtered = df_tarifs_filtered.sort_values(by='Beitrag', ascending=True)
+        df_tarifs_filtered = pd.concat([df_rest, df_acv_komfort, df_ace_comfort_plus]).sort_values(by='Beitrag', ascending=True)
 
         # Farben für die Anbieter definieren
         unique_anbieter = df_tarifs_filtered['Anbietername'].unique()
         colors = ['blue', 'green', 'orange', 'red', 'purple', 'cyan', 'magenta', 'yellow', 'brown', 'pink']
         color_mapping = {anbieter: colors[i % len(colors)] for i, anbieter in enumerate(unique_anbieter)}
-        
+
         # Balkendiagramm erstellen
         bar_fig = go.Figure()
         for i, row in df_tarifs_filtered.iterrows():
@@ -126,16 +114,16 @@ if st.button("Tarife abrufen"):
                 x=[row['Tarifname']],
                 y=[row['Beitrag']],
                 name=row['Anbietername'],
-                marker_color=color_mapping[row['Anbietername']],  # Eindeutige Farbe pro Anbieter
-                width=0.6  # Dicke der Balken
+                marker_color=color_mapping[row['Anbietername']],
+                width=0.6
             ))
         bar_fig.update_layout(title='Balkendiagramm: Tarife vergleichen',
                               xaxis_title='Tarifname',
                               yaxis_title='Beitrag in Euro',
                               xaxis_tickangle=-45,
-                              width=1200,  # Breite des Diagramms erhöhen
-                              height=600)  # Höhe des Diagramms erhöhen
-        bar_fig.update_traces(texttemplate='%{y:.2f} €', textposition='outside')  # Betrag an der Spitze der Balken anzeigen
+                              width=1200,
+                              height=600)
+        bar_fig.update_traces(texttemplate='%{y:.2f} €', textposition='outside')
 
         st.plotly_chart(bar_fig)
 
@@ -147,7 +135,18 @@ if st.button("Tarife abrufen"):
                 y=[row['Beitrag']],
                 mode='lines+markers',
                 name=row['Anbietername'],
-                line=dict(shape='linear', color=color_mapping[row['Anbietername']]),  # Eindeutige Farbe pro Anbieter
+                line=dict(shape='linear', color=color_mapping[row['Anbietername']]),
                 marker=dict(color=color_mapping[row['Anbietername']])
             ))
-        line_fig.update_layout(title='Liniendiagramm: Tar
+        line_fig.update_layout(title='Liniendiagramm: Tarife vergleichen',
+                               xaxis_title='Tarifname',
+                               yaxis_title='Beitrag in Euro',
+                               width=1200,
+                               height=600)
+        line_fig.update_traces(texttemplate='%{y:.2f} €', textposition='top center')
+
+        st.plotly_chart(line_fig)
+
+        # Tabelle anzeigen
+        st.write("Detaillierte Tarifinformationen:")
+        st.dataframe(df_tarifs_filtered)
