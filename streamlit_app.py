4037f5c1455b638e080470a94ba043da3ab3de58
lupes237 <luca.schwieren@googlemail.com>
2024-10-09 11:45:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ region = st.selectbox("Wähle eine Region:", ["Germany", "Europe", "World"])
 person = st.selectbox("Wähle eine Personengruppe:", ["Single", "Pair", "Family"])
 plz = st.text_input("Postleitzahl:", "40210")
 umweltrabatt = st.selectbox("Umweltrabatt:", ["ja", "nein"])
-alter = st.slider("Alter:", 18, 30, 70)
+alter = st.slider("Alter:", 18, 31)
 
 # Button zum Abrufen der Daten
 if st.button("Tarife abrufen"):
